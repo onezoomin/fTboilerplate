7e898f843698d6f3d3c906e263f4526379d5ae51
gotjosh <github@begreen.nu>
2018-06-27 12:38:31 +0100

    add dropdown to example

--- imports/ui/pages/Example.jsx
+++ imports/ui/pages/Example.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Header, Image } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
+
+import { countryOptions } from '../common';
 
 class Example extends React.Component {
+  handleChange(e, { value }) {
+    console.log(this);
+    //  this.value = value;
+  }
+
+  handleClose = (e, valueObj) => {
+    console.log('close: ');
+    console.log(this.value);
+  };
+
   render() {
     return (
       <Header as="h2" textAlign="center">
         <Image src="/ftlogo.png" />
         <p>Example</p>
         <p>(Only visible when logged in.)</p>
+        <Dropdown
+          placeholder="Select Country"
+          fluid
+          multiple
+          search
+          selection
+          onChange={this.handleChange}
+          onClose={this.handleClose}
+          options={countryOptions}
+        />
       </Header>
     );
   }
